e6fe8c2c752d7295ddc69946ae737ad65d9fa543
iSazzad <82577387+iSazzad@users.noreply.github.com>
2025-11-04 12:52:50 +0530

    Change HTTP server to HTTPS server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const path = require("path");
-const { createServer } = require("http");
+const { createServer } = require("https");
 
 const express = require("express");
 const { getIO, initIO } = require("./socket");
