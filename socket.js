8528a6383543d573e2222c7ee2f9230f561c90e8
Sazzad <sazzadhusen.albiorix@gmail.com>
2025-11-07 18:33:29 +0530

    Added endCall, rejectCall, and cancelCall methods

--- socket.js
+++ socket.js
@@ -6,53 +6,79 @@ module.exports.initIO = (httpServer) => {
 
   IO.use((socket, next) => {
     if (socket.handshake.query) {
-      let callerId = socket.handshake.query.callerId;
+      const callerId = socket.handshake.query.callerId;
       socket.user = callerId;
       next();
     }
   });
 
   IO.on("connection", (socket) => {
-    console.log(socket.user, "Connected");
+    console.log(`${socket.user} Connected`);
     socket.join(socket.user);
 
+    // 🔹 Caller initiates a call
     socket.on("call", (data) => {
-      let calleeId = data.calleeId;
-      let rtcMessage = data.rtcMessage;
+      const { calleeId, rtcMessage } = data;
 
       socket.to(calleeId).emit("newCall", {
         callerId: socket.user,
-        rtcMessage: rtcMessage,
+        rtcMessage,
       });
     });
 
+    // 🔹 Callee answers the call
     socket.on("answerCall", (data) => {
-      let callerId = data.callerId;
-      rtcMessage = data.rtcMessage;
+      const { callerId, rtcMessage } = data;
 
       socket.to(callerId).emit("callAnswered", {
         callee: socket.user,
-        rtcMessage: rtcMessage,
+        rtcMessage,
       });
     });
 
+    // 🔹 ICE candidate exchange
     socket.on("ICEcandidate", (data) => {
-      console.log("ICEcandidate data.calleeId", data.calleeId);
-      let calleeId = data.calleeId;
-      let rtcMessage = data.rtcMessage;
-      console.log("socket.user emit", socket.user);
-
+      const { calleeId, rtcMessage } = data;
       socket.to(calleeId).emit("ICEcandidate", {
         sender: socket.user,
-        rtcMessage: rtcMessage,
+        rtcMessage,
       });
+    });
+
+    // 🔹 Caller cancels before callee answers
+    socket.on("cancelCall", (data) => {
+      const { calleeId } = data;
+      console.log(`${socket.user} canceled call to ${calleeId}`);
+
+      socket.to(calleeId).emit("callCanceled", {
+        callerId: socket.user,
+      });
+    });
+
+    // 🔹 Callee rejects incoming call
+    socket.on("rejectCall", (data) => {
+      const { callerId } = data;
+      console.log(`${socket.user} rejected call from ${callerId}`);
+
+      socket.to(callerId).emit("callRejected", {
+        calleeId: socket.user,
+      });
+    });
+
+    // End Call by user disconnects
+    socket.on("endCall", (data) => {
+      const { calleeId } = data;
+      console.log(`Call ended by ${socket.user} for ${calleeId}`);
+
+      // Notify the other side
+      socket.to(calleeId).emit("callEnded", { sender: socket.user });
     });
   });
 };
 
 module.exports.getIO = () => {
   if (!IO) {
-    throw Error("IO not initilized.");
+    throw Error("IO not initialized.");
   } else {
     return IO;
   }
